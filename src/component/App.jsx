4d6e5fce39da122f9e9ec6bca196e3d06ca433e9
mmvli <mmvlivo@gmail.com>
2020-03-12 19:23:26 +0200

    movies

---
+++ src/component/App.jsx
@@ -0,0 +1,57 @@
+import React, {Component} from "react";
+// 8.5K (gzipped: 3.4K)
+import {moviesData} from "../moviesData";
+import MovieList from "./MovieList";
+import MovieListWillWatch from "./MovieListWillWatch";
+
+// console.log(moviesData);
+
+
+
+class App extends Component {
+    constructor() {
+        super();
+
+        this.state = {
+            movies: moviesData,
+            moviesWillWatch: []
+        };
+    }
+
+    addMovieToWillWatch = movie => {
+        this.setState( prevState =>({
+            moviesWillWatch: [...prevState.moviesWillWatch, movie]
+        }));
+    };
+    removeMovieFromWillWatch = movie => {
+        this.setState(prevState => ({
+            moviesWillWatch: prevState.moviesWillWatch.filter(
+                item => item.id !== movie.id
+            )
+        }));
+    };
+
+    render() {
+        const {movies, moviesWillWatch} = this.state;
+        return  (
+            <div className="container">
+                <div className="row mt-4">
+                    <div className="col_9">
+                        <MovieList
+                            movies={movies}
+                            addMovieToWillWatch={this.addMovieToWillWatch}
+                            removeMovieFromWillWatch={this.removeMovieFromWillWatch}
+                        />
+                    </div>
+                    <div className="col-3">
+                        <MovieListWillWatch moviesWillWatch={moviesWillWatch} />
+                    </div>
+                </div>
+            </div>
+        );
+    }
+
+}
+
+export default App;
+
--- src/index.js
+++ src/index.js
@@ -1,12 +1,95 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from 'react'; // 8.5K (gzipped: 3.4K)
+import ReactDOM from 'react-dom'; //109.7K (gzipped: 35.2K)
+import App from "./component/App";
+import 'bootstrap/dist/css/bootstrap.min.css';// 154K  (gzipped)
+
+// import './index.css';
+
+// import moviesData from "./moviesData";
+//
+// // console.log(moviesData)
+// // import App from './App';
+// // import * as serviceWorker from './serviceWorker';
+// const movie ={
+//     title: "Avengers: Infinity War",
+//     vote_average: 8.5,
+//     image: "https://kino-teatr.ua/public/main/films/2019-07/x2_poster_5d1c544d25c1e.jpg",
+//     overview: "Описание фильма Приключения маленького Реми"
+//
+// }
+//
+// function Image (props) {
+//     console.log("Image props", props);
+//     return (
+//         <img src={props.src} alt={props.alt}/>
+//     )
+// }
+//
+// // function MovieItem(props) {
+// //     console.log("props = ", props);
+// //     const {data: {title, vote_average, image} } = props;
+// //     return (
+// //         <div>
+// //             <img src={image} alt={title}/>
+// //             <p>{title}</p>
+// //             <p>{vote_average}</p>
+// //         </div>
+// //     );
+// // }
+//
+// class MovieItem extends  React.Component {
+//     constructor() {
+//         super();
+//
+//         this.state = {
+//             show: false,
+//             like: false
+//         }
+//     }
+//
+//     toggleOverview = () => {
+//         this.setState({
+//             show: !this.state.show
+//         });
+//     }
+//     handleLike = () => {
+//         this.setState({
+//             like : !this.state.like
+//         })
+//     }
+//
+//     render() {
+//         const {data: {title, vote_average, image,overview} } = this.props;
+//         console.log("state",this.state);
+//         return (
+//             <div style={{width: "250px"}}>
+//                 <img style={{width: "100%"}} src={image} alt={title}/>
+//                 <p>{title}</p>
+//                 <p>{vote_average}</p>
+//                 <div
+//                     style={{display: "flex", justifyContent: "space-between"}}>
+//                 <button  className={this.state.like ? "btn-like" : ""} type="button" onClick={this.handleLike}
+//
+//                 >Like</button>
+//                 <button type="button" onClick={this.toggleOverview}
+//                 >{this.state.show ? "Hide" : "Show"}
+//                 </button>
+//                 </div>
+//                 {this.state.show ? <p>{overview}</p> : null}
+//             </div>
+//         )
+//     }
+// }
+//
+// function App() {
+//     return (
+//         <div>
+//             <MovieItem data = {movie}/>
+//         </div>
+//     );
+// }
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+
+
